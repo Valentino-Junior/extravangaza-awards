c3ee8966f95ebf5269d055fc49fb9b7833f60de2
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-12-14 05:45:23 +0300

    Installed crispy template pack

--- extravaganza/settings.py
+++ extravaganza/settings.py
@@ -151,5 +151,10 @@ cloudinary.config(
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
