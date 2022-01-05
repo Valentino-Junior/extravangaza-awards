cb19919d93f7dcd2e63405b21d6babfd23580b21
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2022-01-05 10:04:27 +0300

    Added template processor

--- extravaganza/settings.py
+++ extravaganza/settings.py
@@ -112,6 +112,22 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
+TEMPLATES = [
+    {
+    
+        'OPTIONS': {
+        
+            'context_processors': [
+                
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
+                
+            ]
+        }
+    }
+]
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
